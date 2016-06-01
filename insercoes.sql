1e68973f2d15839b2e702b042b03ede8c76fe8e4
ednamartins <Eliedygaspar@gmail.com>
2016-05-31 21:36:47 -0300

    insersoes

---
+++ insercoes.sql
@@ -0,0 +1,5 @@
+insert into pessoa values ('A2020','Edna', 'Patricia Gaspar', 'Martins','ed@gmail.com','bantu','f','claudia gaspar','Aurelio Maritins','07-Mach-1994','luanda','Angola','angolana');
+insert into pessoa values ('B2020','Claud', 'Marisa', 'Gaspar','cl@gmail.com','india','f','Francisca Santos','João Gaspar','11-Out-1976','Praia','Cabo verde','Caboverdiana');
+
+insert into endereco(12,'Alameda'
+);
