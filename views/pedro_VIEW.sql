08e799f2e2793e3dcc430ca5e77b9ad6ad0a9abb
Pedro Garcia <garcia.ph@gmail.com>
2016-06-14 20:08:55 -0300

    Views testadas e funcionando corretamente

--- views/pedro_VIEW.sql
+++ views/pedro_VIEW.sql
@@ -6,26 +6,26 @@ FROM Evento AS e, Calendario AS c
 WHERE e.calendario_data = c.data_inicio AND
 	  e.calendario_tipo = c.tipo AND
 	  c.tipo = 'p'
-GROUP BY e.data_inicio;
+ORDER BY e.data_inicio, e.data_fim;
 
 
 -- View de eventos do calendario do Ensino à Distância
-CREATE OR REPLACE VIEW view_eventoscalendariopresencial
+CREATE OR REPLACE VIEW view_eventoscalendarioead
 AS
 SELECT e.data_inicio AS "Data de Início", e.data_fim AS "Data de Término", e.descricao AS "Descrição"
 FROM Evento AS e, Calendario AS c
 WHERE e.calendario_data = c.data_inicio AND
 	  e.calendario_tipo = c.tipo AND
 	  c.tipo = 'e'
-GROUP BY e.data_inicio;
+ORDER BY e.data_inicio, e.data_fim;
 
 
 -- View de eventos do calendario Administrativo
-CREATE OR REPLACE VIEW view_eventoscalendariopresencial
+CREATE OR REPLACE VIEW view_eventoscalendarioadministrativo
 AS
 SELECT e.data_inicio AS "Data de Início", e.data_fim AS "Data de Término", e.descricao AS "Descrição"
 FROM Evento AS e, Calendario AS c
 WHERE e.calendario_data = c.data_inicio AND
 	  e.calendario_tipo = c.tipo AND
 	  c.tipo = 'a'
-GROUP BY e.data_inicio;
+ORDER BY e.data_inicio, e.data_fim;
