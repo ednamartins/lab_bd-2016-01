1ccf5109a60e6597500ea227d4570b157f7ffc00
Jéssica Caroline <jc.jooy@gmail.com>
2016-06-15 01:11:54 -0300

    Update jessicaDias_INSERT.sql

--- scripts/jessicaDias_INSERT.sql
+++ scripts/jessicaDias_INSERT.sql
@@ -1,7 +1,7 @@
 -- Nome, website, sigla(PK), telefone1, telefone2, endereço 
 INSERT INTO campus VALUES ('Universidade Federal de São Carlos', 'http://www2.ufscar.br/home/index.php', 'UFSCARSC', '16 3216-8520','', 'Rua dos passarinhos 1');
 INSERT INTO campus VALUES ('Universidade Federal de São Carlos', 'http://www2.ufscar.br/home/index.php', 'UFSCARARA', '16 3216-8530','', 'Rua dos passarinhos 2');
-INSERT INTO campus VALUES ('Universidade Federal de São Carlos', 'http://www2.ufscar.br/home/index.php', 'UFSCARSOR', '16 3216-8540','', 'Rua dos passarinhos 3');
+INSERT INTO campus VALUES ('Universidade Federal de São Carlos', 'http://www2.ufscar.br/home/index.php', 'UFSCARSO', '16 3216-8540','', 'Rua dos passarinhos 3');
 INSERT INTO campus VALUES ('Universidade Federal de São Carlos', 'http://www2.ufscar.br/home/index.php', 'UFSCARLAG', '16 3216-8550','', 'Rua dos passarinhos 4');
 INSERT INTO campus VALUES ('Universidade Federal de São Carlos', 'http://www2.ufscar.br/home/index.php', 'UFSCAR', '16 3216-8560','', 'Rua dos passarinhos 5');
 
@@ -13,12 +13,12 @@ INSERT INTO centro VALUES ('Centro de Ciências Agrárias', 'http://www.cca.ufsc
 INSERT INTO centro VALUES ('Centro de Qualquer Coisa', 'http://www.cqc.ufscar.br/', '10,50', 'CQC', '16 3315-1111');
 
 
--- Nome, website, sigla(PK), sigla_campus(FK), telefone1, telefone2, endereço, sigla_centro 
-INSERT INTO departamento VALUES ('Computação', 'http://www2.ufscar.br/home/index.php', 'DC', '16 3414-6582','', 'Estrada 1', 'CCET');
-INSERT INTO departamento VALUES ('Biologia', 'http://www2.ufscar.br/home/index.php', 'DBIO', '16 3414-1010','', 'Estrada 2', 'CCBS');
-INSERT INTO departamento VALUES ('Ciências Sociais', 'http://www2.ufscar.br/home/index.php', 'DCS', '16 3414-2020','', 'Estrada 3', 'CECH');
-INSERT INTO departamento VALUES ('Agronomia', 'http://www2.ufscar.br/home/index.php', 'DAG', '16 3414-3036','', 'Estrada 4', 'CCA');
-INSERT INTO departamento VALUES ('Deboa', 'http://www2.ufscar.br/home/index.php', 'DBOA', '16 3414-0000','', 'Estrada 5', 'CQC');
+-- Nome, website, sigla(PK), telefone1, telefone2, endereço, sigla_campus(FK) 
+INSERT INTO departamento VALUES ('Computação', 'http://www2.ufscar.br/home/index.php', 'DC', '16 3414-6582','', 'Estrada 1', 'UFSCARSC');
+INSERT INTO departamento VALUES ('Biologia', 'http://www2.ufscar.br/home/index.php', 'DBIO', '16 3414-1010','', 'Estrada 2', 'UFSCARARA');
+INSERT INTO departamento VALUES ('Ciências Sociais', 'http://www2.ufscar.br/home/index.php', '16 3414-2020','', 'Estrada 3', 'UFSCARSO');
+INSERT INTO departamento VALUES ('Agronomia', 'http://www2.ufscar.br/home/index.php', 'DAG', '16 3414-3036','', 'Estrada 4', 'UFSCARLAG');
+INSERT INTO departamento VALUES ('Deboa', 'http://www2.ufscar.br/home/index.php', 'DBOA', '16 3414-0000','', 'Estrada 5', 'UFSCAR');
 
 -- Número de créditos, nome (PK) 
 INSERT INTO atcomp VALUES (60, 'IC');
