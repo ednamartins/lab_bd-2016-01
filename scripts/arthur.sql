d52b96b54b958c997b03b13671ac8f164078152b
Arthur Munhoz <arthurmunhoz@hotmail.com>
2016-06-15 22:43:27 -0300

    Update arthur.sql

--- scripts/arthur.sql
+++ scripts/arthur.sql
@@ -60,10 +60,60 @@ CREATE TABLE Estagia
 );
 
 
-INSERT INTO estagia VALUES('10/05/2016', '10/11/2016', 06562165849, ROW(Ronaldo, 25145885696), ROW(Ademar, 154448556225), 'Carta Avaliacao', 'Termo Compromisso');
+---------------
+-- Estudante --
+---------------
+-- Entidade do Kyioto , porem necessario devido à dependencia de tabelas
 
-INSERT INTO empresa VALUES(2566856, Samsung, ROW('Rua da Pamonha', 'Sem complemento', 'Cidade Jardim', 'Jaguariúna', 'SP', 'Brasil', 13654987));
+INSERT INTO Estudante VALUES(407704,'41879402866','2010','10000','s', true, false);
 
-INSERT INTO turma VALUES(0258, ‘{212, 235}’, 120);
+INSERT INTO Estudante VALUES(564898,'65423215899','1999','16000','s', false, true);
 
-INSERT INTO curso VALUES(0212, 'www.dc.ufscar.br', 'Ciência da Computação', ROW('Joao Silva', ROW('Sala', 'Fixo', 05, 16, 34521443)));
+INSERT INTO Estudante VALUES(985589,'84565878502','2005','12335','n',false, true);
+
+INSERT INTO Estudante VALUES(379409,'84565878504','2005','12335','n',false, true);
+
+INSERT INTO Estudante VALUES(408000,'84565878503','2005','12335','n',false, true);
+
+
+-------------
+-- Empresa --
+-------------
+
+INSERT INTO empresa VALUES(2566856, 'Samsung', ROW('Rua da Samsung', 'Sem complemento', 'Centro', 'Jaguariúna', 'SP', 'Brasil', 13654987));
+
+INSERT INTO empresa VALUES(2566855, 'Sony', ROW('Rua da Sony', 'Sem complemento', 'Centro', 'Campinas', 'SP', 'Brasil', 13654986));
+
+INSERT INTO empresa VALUES(2566854, 'Panasonic', ROW('Rua da Panasonic', 'Sem complemento', 'Tijuco Preto', 'Sao Carlos', 'SP', 'Brasil', 13654985));
+
+INSERT INTO empresa VALUES(2566853, 'Geico', ROW('Rua da Geico', 'Sem complemento', 'Ita', 'Jaguariúna', 'SP', 'Brasil', 13654984));
+
+INSERT INTO empresa VALUES(2566852, 'Toshiba', ROW('Rua da Toshiba', 'Sem complemento', 'Centro', 'Jaguariúna', 'SP', 'Brasil', 13654983));
+
+-------------
+-- Estagia --
+-------------
+
+INSERT INTO estagia VALUES('2016-02-20', '2016-06-21', '41879402866', 2566856, ROW(25145885696, 'Ronaldo'), ROW(154448556225, 'Ademar'), 'Carta Avaliacao', 'Termo Compromisso');
+
+INSERT INTO estagia VALUES('2016-04-28', '2016-10-09', '65423215899', 2566855, ROW(25145885697, 'Jose'), ROW(154448556225, 'Ademar'), 'Carta Avaliacao1', 'Termo Compromisso1');
+
+INSERT INTO estagia VALUES('2016-04-10', '2015-12-13', '84565878502', 2566854, ROW(25145885695, 'Carlos'), ROW(154448556225, 'Ademar'), 'Carta Avaliacao2', 'Termo Compromisso2');
+
+INSERT INTO estagia VALUES('2016-02-20', '2016-10-07', '84565878504', 2566853, ROW(25145885694, 'Arthur'), ROW(154448556225, 'Ademar'), 'Carta Avaliacao3', 'Termo Compromisso3');
+
+INSERT INTO estagia VALUES('2016-02-03', '2016-12-11', '84565878503', 2566852, ROW(25145885693, 'Joaquina'), ROW(154448556225, 'Ademar'), 'Carta Avaliacao4', 'Termo Compromisso4');
+
+
+-------------
+--- Curso ---
+-------------
+INSERT INTO curso VALUES(0212, 'www.dc.ufscar.br/~bcc', 'Ciência da Computação', ROW('Indiana Jones', ROW('Sala1', 'Fixo', 05, 16, 34521452)));
+
+INSERT INTO curso VALUES(0213, 'www2.dc.ufscar.br/~enc', 'Engenharia da Computação', ROW('He-man', ROW('Sala2', 'Fixo', 04, 16, 34521443)));
+
+INSERT INTO curso VALUES(0214, 'www.dm.ufscar.br', 'Matematica', ROW('Aristoteles', ROW('Sala8', 'Fixo', 06, 16, 34521474)));
+
+INSERT INTO curso VALUES(0215, 'www.dep.ufscar.br', 'Engenharia de Producao', ROW('Mario Bros', ROW('Sala', 'Fixo', 01, 16, 34521485)));
+
+INSERT INTO curso VALUES(0216, 'www.dpsi.ufscar.br', 'Psicologia', ROW('James Bond', ROW('Sala007', 'Fixo', 02, 16, 34521007)));
