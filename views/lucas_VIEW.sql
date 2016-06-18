f7600fbea6ba64ba0a250aa95b16c961e719fe7b
Lucas Antoniale Callegari <lucascallegari93@gmail.com>
2016-06-18 15:34:59 -0300

    Add files via upload

---
+++ views/lucas_VIEW.sql
@@ -0,0 +1,8 @@
+﻿CREATE OR REPLACE VIEW view_nomesdisciplinasprerequisitos
+AS
+SELECT d.nome AS "Nome Disciplina Pré-Requisito", d2.nome AS "Nome Disciplina"
+FROM Disciplina AS d, Disciplina AS d2, DisciplinaPreReq AS dp
+WHERE d.codigo = dp.codigoPreRequisito AND d2.codigo = dp.Disciplina_codigo;
+--ORDER BY d.nome;
+
+SELECT * FROM view_nomesdisciplinasprerequisitos;
