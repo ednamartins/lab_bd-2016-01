0bb9d21ae083de9cdd3a62ff87145f6a571a0998
Lucas Antoniale Callegari <lucascallegari93@gmail.com>
2016-06-18 15:36:42 -0300

    Add files via upload

--- scripts/lucas.sql
+++ scripts/lucas.sql
@@ -1,44 +1,111 @@
 DROP TABLE DisciplinaPreReq;
+DROP TABLE Disciplina;
 
-CREATE TABLE DisciplinaPreReq(
-	Codigo CHAR not null(10),
-	Sigla CHAR(10),
-	Nro_creditos INTEGER,
-	Categoria CHAR(20),
-	CodigoPreRequisito CHAR (10),
-	CONSTRAINT Disciplina_PK primary key (Codigo),
-	CONSTRAINT Disciplina_FK foreing key (CodigoPreRequisito) references Disciplina
+CREATE TABLE Disciplina 
+(
+	codigo character varying(10) CONSTRAINT Disciplina_PK primary key,
+	nome character varying(50),
+	nro_creditos integer,
+	vezes_Realizadas integer,
+	categoria character varying(20)	
 );
 
-INSERT INTO DisciplinaPreReq VALUES(
-	'08801',
-	'Calculo 1', 
+CREATE TABLE DisciplinaPreReq
+(
+	Disciplina_codigo character varying(10) not null,
+	codigoPreRequisito character varying (10) not null,
+
+	CONSTRAINT Disciplina_FK foreign key (Disciplina_codigo) references Disciplina (codigo),
+	CONSTRAINT PreReq_FK foreign key (codigoPreRequisito) references Disciplina(codigo),
+	CONSTRAINT DisciplinaPreReq_pk primary key (Disciplina_codigo, codigoPreRequisito)
+);
+
+INSERT INTO Disciplina VALUES(
+	'08806',
+	'Calculo 1',
 	4,
+	0,
 	'Obrigatoria'
+	
+);
+
+INSERT INTO Disciplina VALUES(
+	'08807',
+	'Calculo 2',
+	4,
+	0,
+	--Vezes_Realizadas INT,
+	--DataHorario DateTime, --(Não é atributo de turma?)
+	'Obrigatoria'
+	
+);
+
+INSERT INTO Disciplina VALUES(
+	'08808',
+	'Calculo 3',
+	4,
+	0,
+	'Obrigatoria'
+	
+);
+
+INSERT INTO Disciplina VALUES(
+	'08809',
+	'Calculo 4',
+	4,
+	0,
+	'Obrigatoria'
+	
+);
+
+INSERT INTO Disciplina VALUES(
+	'08810',
+	'Calculo Numerico',
+	4,
+	0,
+	'Obrigatoria'
+	
+);
+
+INSERT INTO Disciplina VALUES(
+	'562311',
+	'BD',
+	2,
+	0,
+	'Obrigatoria'
+	
+);
+
+INSERT INTO Disciplina VALUES(
+	'562312',
+	'LabBD',
+	2,
+	0,
+	'Obrigatoria'
+	
 );
 
 INSERT INTO DisciplinaPreReq VALUES(
-	'08802',
-	'Calculo 2', 
-	4,
-	'Obrigatoria',
-	'08801'
+	'08807',
+	'08806'
 );
 
 INSERT INTO DisciplinaPreReq VALUES(
-	'08803',
-	'Calculo 3', 
-	4,
-	'Obrigatoria',
-	'08802'
+	'08808',
+	'08807'
 );
 
 INSERT INTO DisciplinaPreReq VALUES(
-	'08804',
-	'Calculo 4', 
-	4,
-	'Obrigatoria',
-	'08803'
+	'08809',
+	'08808'
 );
 
+INSERT INTO DisciplinaPreReq VALUES(
+	'08810',
+	'08806'
+);
 
+INSERT INTO DisciplinaPreReq VALUES(
+	'562312',
+	'562311'
+);
