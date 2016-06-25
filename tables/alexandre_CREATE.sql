e58e1c7d16ff7a6ef03caec2c06319bd4736af55
xandjiji <xand@hotmail.com.br>
2016-06-25 20:03:15 -0300

    Update alexandre_CREATE.sql

--- tables/alexandre_CREATE.sql
+++ tables/alexandre_CREATE.sql
@@ -8,5 +8,9 @@ CREATE TYPE gradecurricular AS (
 
 CREATE TABLE ProjetoPoliticoPedagogico(
 	grade gradecurricular
-	FOREIGN KEY (Codigo_curso) REFERENCES Curso(Codigo_curso)
+	ConselhoCurso_id integer NOT NULL, -- Conselho de Curso que define PPP
+	Curso_codigo integer, -- Curso que possui PPP
+	CONSTRAINT ProjetoPoliticoPedagogico_ConselhoCurso_fk FOREIGN KEY (ConselhoCurso_id) REFERENCES ConselhoCurso (id),
+	CONSTRAINT ProjetoPoliticoPedagogico_Curso_fk FOREIGN KEY (Curso_codigo) REFERENCES Curso (codigo),
+	CONSTRAINT ProjetoPoliticoPedagogico_pk PRIMARY KEY (ConselhoCurso_id, grade)
 );
