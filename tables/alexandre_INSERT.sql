2017a48d5aacbe6ec23aa8585582861e8a6c0332
xandjiji <xand@hotmail.com.br>
2016-06-25 18:47:19 -0300

    Create alexandre_INSERT.sql

---
+++ tables/alexandre_INSERT.sql
@@ -0,0 +1,5 @@
+-- Projeto Politico Pedagogico
+
+INSERT INTO ProjetoPoliticoPedagogico VALUES("true", "false", "false");
+INSERT INTO ProjetoPoliticoPedagogico VALUES("false", "true", "false");
+INSERT INTO ProjetoPoliticoPedagogico VALUES("false", "false", "true");
