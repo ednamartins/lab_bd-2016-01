550e9888cf293f61f9238301b93f93c36cf1dc2b
xandjiji <xand@hotmail.com.br>
2016-06-25 18:45:20 -0300

    Update alexandre.sql

--- tables/alexandre.sql
+++ tables/alexandre.sql
@@ -10,15 +10,3 @@ CREATE TABLE ProjetoPoliticoPedagogico(
 	grade gradecurricular
 	FOREIGN KEY (Codigo_curso) REFERENCES Curso(Codigo_curso)
 );
-
-INSERT INTO ProjetoPoliticoPedagogico VALUES(
-	ROW("true", "false", "false")
-);
-
-INSERT INTO ProjetoPoliticoPedagogico VALUES(
-	ROW("false", "true", "false")
-);
-
-INSERT INTO ProjetoPoliticoPedagogico VALUES(
-	ROW("false", "false", "true")
-);
